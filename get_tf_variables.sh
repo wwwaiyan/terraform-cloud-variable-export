18e9fe1ddcfaf2c912f25486b2ea082409ff6957
Wai Yan Soe <49969559+wwwaiyan@users.noreply.github.com>
2024-12-06 10:42:03 +0630

    feat: use default token

--- get_tf_variables.sh
+++ get_tf_variables.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-## User Input for Terraform Cloud
-read -sp "Enter the Terraform Cloud API Token: " TFC_TOKEN
-echo
+
+## Function to get the default token from credentials.tfrc.json
+get_default_token() {
+  local TOKEN_FILE="$HOME/.terraform.d/credentials.tfrc.json"
+  if [[ -f "$TOKEN_FILE" ]]; then
+    jq -r '.credentials["app.terraform.io"].token' "$TOKEN_FILE"
+  else
+    echo ""
+  fi
+}
+
+## Prompt user to use default token or provide a new one
+read -p "Do you want to use the default Terraform login token? (yes/no): " USE_DEFAULT_TOKEN
+
+if [[ "$USE_DEFAULT_TOKEN" == "yes" ]]; then
+  TFC_TOKEN=$(get_default_token)
+  if [[ -z "$TFC_TOKEN" ]]; then
+    echo "Error: Default token not found in credentials.tfrc.json. Please log in using 'terraform login' or provide a token manually."
+    exit 1
+  fi
+else
+  read -sp "Enter the Terraform Cloud API Token: " TFC_TOKEN
+  echo
+fi
+
+## Prompt for other inputs
 read -p "Enter the Organization Name: " ORG_NAME
 read -p "Enter the Workspace Name: " WORKSPACE_NAME
-read -p "Enter the tfvars file name (press Enter for default '${WORKSPACE_NAME}-terraform-variables.tfvars'): " TFVARS_FILE
+read -p "Enter the tfvars file name (press Enter for default '${WORKSPACE_NAME}-terraform.tfvars'): " TFVARS_FILE
 
 # Set default value for TFVARS_FILE if not provided
 TFVARS_FILE=${TFVARS_FILE:-"${WORKSPACE_NAME}-terraform-variables.tfvars"}
@@ -31,4 +54,4 @@ curl \
   jq -r '.data[] | select(.attributes.category=="terraform") | "\(.attributes.key) = \"\(.attributes.value)\""' > "$TFVARS_FILE"
 
 echo "Terraform variables have been saved to $TFVARS_FILE."
-echo "Don't forget to cleanup variables files after use."
+echo "Don't forget to clean up variables files after use."
